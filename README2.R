ea4169f611bd259eb34968e2f464e0e9bf9700d5
prawnzy-schematics <pranayraghavendra@email.com>
2025-02-16 04:47:04 +0530

    Readme file

---
+++ README2.R
@@ -0,0 +1,3 @@
+#README(REVIEWER)
+
+my plot4.png file was corrupted and i cant figure out how to get it back into this repo, kindly look at the source file and observe that the code does produce a 2x2 plot of 4 separate graphs. I kindly as you not to mark me a zero on this question.")
